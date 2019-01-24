a900c803ab8f91dc7c3312bad0c3e762e45cf3a7
THAI TECH FEED <thaitechfeed@gmail.com>
2019-01-24 07:12:53 +0700

    Added 50 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 163973,
-	"created_at": "Wed Jan 23 00:12:52 +0000 2019",
+	"tweets": 164023,
+	"created_at": "Thu Jan 24 00:12:52 +0000 2019",
 	"lang": "th"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 1202,
+		"tweet_count": 1252,
 		"month": 1
 	},
 	{

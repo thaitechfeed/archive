971246f7ca8e2d55e3a3d718be4f9c129147704b
THAI TECH FEED <thaitechfeed@gmail.com>
2019-05-27 07:12:52 +0700

    Added 26 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 170620,
-	"created_at": "Sun May 26 00:12:51 +0000 2019",
+	"tweets": 170646,
+	"created_at": "Mon May 27 00:12:51 +0000 2019",
 	"lang": "th"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_05.js",
 		"year": 2019,
 		"var_name": "tweets_2019_05",
-		"tweet_count": 1380,
+		"tweet_count": 1406,
 		"month": 5
 	},
 	{

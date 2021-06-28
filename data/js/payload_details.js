2c821f512f85595c703f70027fd629b6e5121617
THAI TECH FEED <thaitechfeed@gmail.com>
2021-06-28 07:12:58 +0700

    Added 11 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 209932,
-	"created_at": "Sun Jun 27 00:12:54 +0000 2021",
+	"tweets": 209943,
+	"created_at": "Mon Jun 28 00:12:55 +0000 2021",
 	"lang": "th"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_06.js",
 		"year": 2021,
 		"var_name": "tweets_2021_06",
-		"tweet_count": 1469,
+		"tweet_count": 1480,
 		"month": 6
 	},
 	{

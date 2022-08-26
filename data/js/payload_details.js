8f09211bd6655f402f4beecc6fabb1c4df28b9d7
THAI TECH FEED <thaitechfeed@gmail.com>
2022-08-26 07:12:51 +0700

    Added 56 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 229483,
-	"created_at": "Thu Aug 25 00:12:51 +0000 2022",
+	"tweets": 229539,
+	"created_at": "Fri Aug 26 00:12:50 +0000 2022",
 	"lang": "th"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_08.js",
 		"year": 2022,
 		"var_name": "tweets_2022_08",
-		"tweet_count": 1136,
+		"tweet_count": 1192,
 		"month": 8
 	},
 	{
